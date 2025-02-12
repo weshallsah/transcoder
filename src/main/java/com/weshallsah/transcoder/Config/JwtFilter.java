ef74f8b1423a58443cfec36c1f45123c384bfa3f
weshallsah <vishalk74064@gmail.com>
2025-02-13 00:43:44 +0530

    token verification works

--- src/main/java/com/weshallsah/transcoder/Config/JwtFilter.java
+++ src/main/java/com/weshallsah/transcoder/Config/JwtFilter.java
@@ -7,15 +7,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.annotations.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtFilter extends OncePerRequestFilter {
 
     @Autowired
@@ -29,10 +32,12 @@ public class JwtFilter extends OncePerRequestFilter {
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
+
         if(authHeader != null && authHeader.startsWith("Bearer ")){
             token = authHeader.substring(7);
             username = services.getusername(token);
         }
+
         if(username !=null && SecurityContextHolder.getContext().getAuthentication() == null){
             UsersDetails usersDetails = (UsersDetails) applicationContext.getBean(userServices.class).loadUserByUsername(username);
             if(services.validateToken(token,usersDetails)){
